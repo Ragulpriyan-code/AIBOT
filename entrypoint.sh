08553fa6226535720259a922eacabeb3faa1676c
Ragul Priyan <ragulpriyan.m@techjays.com>
2026-01-09 12:07:47 +0530

    Force gunicorn to ignore config files and fix PORT

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -e
 
-echo "Starting Gunicorn on port $PORT"
+echo "PORT value is: $PORT"
 
 exec gunicorn chatbotapp.wsgi:application \
+  --config /dev/null \
   --bind 0.0.0.0:${PORT:-8000} \
   --workers 2 \
   --threads 4 \
